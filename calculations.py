e9029b50a9bcf924c32cd9c4ec148c6b44f26659
James Chase <gochases@gmail.com>
2023-10-17 13:08:15 -0600

    Add calculations.py file to use for extracting methods to be tested.

---
+++ calculations.py
@@ -0,0 +1,12 @@
+######################################################################################################################################################
+# Name: Jamaes A. Chase
+# File: calculations.py
+# Date: 17 October 2023
+# Description:
+#
+# Module file holding functions that aren't members of the Engine class.
+#
+######################################################################################################################################################
+
+if __name__ == '__main__':
+    assert False, '\n\nThis is a module of functions. Please import its contents into another file.\n'
