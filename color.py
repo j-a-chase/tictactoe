0901dc3427c61b645ac85029952e381e527b7718
James Chase <gochases@gmail.com>
2023-10-16 20:09:00 -0600

    Add color.py file to handle obtaining colors for the game.

---
+++ color.py
@@ -0,0 +1,14 @@
+######################################################################################################################################################
+# Name: James A. Chase
+# File: color.py
+# Date: 5 October 2023
+######################################################################################################################################################
+
+# imports
+from typing import Tuple
+from random import randint
+
+
+
+if __name__ == '__main__':
+    assert False, '\n\nThis is a module, please import its contents into another file.\n'
